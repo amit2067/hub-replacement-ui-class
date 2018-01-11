56646e5866a86faaa47dec6ced6e94250b37d1d3
amit2067 <amit2067@gmail.com>
2018-01-11 09:45:03 +0000

    Update ClassController.java

--- src/main/java/uk/syntel/hackathon/clas/app/controller/ClassController.java
+++ src/main/java/uk/syntel/hackathon/clas/app/controller/ClassController.java
@@ -5,19 +5,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import uk.syntel.hackathon.clas.app.beans.Application;
 import uk.syntel.hackathon.clas.app.beans.Customer;
 import uk.syntel.hackathon.clas.app.service.ClassService;
 
+@Api(value="CLASS Service", description="CLASS Operations to deal with Customers and Applications")
 @RestController
 public class ClassController {
 
 	@Autowired
-	ClassService service;
+	private ClassService service;
 
+	@ApiOperation(value = "Create a Customer in CLASS",response = Customer.class)
 	@PostMapping("/createCustomer")
 	public Customer createCustomer(@RequestBody Customer customer) {
 		Customer createdCustomer = service.createCustomer(customer);
 		return createdCustomer;
 	}
+	
+	@ApiOperation(value = "Create an Application in CLASS",response = Application.class)
+	@PostMapping("/createApplication")
+	public Application createApplication(@RequestBody Application application) {
+		Application createdApp = service.createApplication(application);
+		return createdApp;
+	}	
 
 }
