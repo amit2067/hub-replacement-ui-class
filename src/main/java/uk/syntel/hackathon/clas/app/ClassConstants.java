14b4f789ea6c4d1ac1af689bf6820b4f4bc7c98c
amit2067 <amit2067@gmail.com>
2018-01-11 09:50:03 +0000

    Create ClassConstants.java

---
+++ src/main/java/uk/syntel/hackathon/clas/app/ClassConstants.java
@@ -0,0 +1,10 @@
+package uk.syntel.hackathon.clas.app.util;
+
+public interface ClassConstants {
+	
+	String CUSTOMER_PUBLISH_URL = "http://CZC3392FZ7.SLC.CO.UK:9205/kpublisher/publishCust";
+	String APPLICATION_PUBLISH_URL = "http://CZC3392FZ7.SLC.CO.UK:9205/kpublisher/publishApp";
+	String CLASS = "Class";	
+	String ELASTIC_PUBLISH_URL = "http://czc4464xb3.slc.co.uk:9183/createLog";
+
+}
