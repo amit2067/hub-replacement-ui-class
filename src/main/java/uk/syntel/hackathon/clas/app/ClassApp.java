6a3477d2b34b43d0c83f2270a6ddaddc9a0452ed
amit2067 <amit2067@gmail.com>
2018-01-11 09:46:58 +0000

    Update ClassApp.java

--- src/main/java/uk/syntel/hackathon/clas/app/ClassApp.java
+++ src/main/java/uk/syntel/hackathon/clas/app/ClassApp.java
@@ -2,7 +2,9 @@ package uk.syntel.hackathon.clas.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"uk.syntel.hackathon.clas.app"})
@@ -11,5 +13,10 @@ public class ClassApp
     public static void main( String[] args )
     {
         SpringApplication.run(ClassApp.class, args);
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
     }
 }
