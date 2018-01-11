106db6892cb26a90dcc92774062c57f9143ea463
amit2067 <amit2067@gmail.com>
2018-01-11 09:52:19 +0000

    Update class.js

--- src/main/resources/static/js/class.js
+++ src/main/resources/static/js/class.js
@@ -4,6 +4,9 @@ classApp.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/addNewCustomer', {
 		templateUrl : 'classAddCustomer.html',
 		controller : 'classController'
+	}).when('/createApplication', {
+		templateUrl : 'classCreateApp.html',
+		controller : 'classController'
 	}).otherwise({
 		templateUrl : 'classAddCustomer.html',
 		controller : 'classController'
@@ -23,16 +26,67 @@ classApp.factory('classService', [
 						console.error('Error while creating customer');
 						return $q.reject(errResponse);
 					});
+				},
+				createApplication : function(application) {
+					return $http.post('http://localhost:9083/createApplication',
+							application).then(function(response) {
+						return response.data;
+					}, function(errResponse) {
+						console.error('Error while creating application');
+						return $q.reject(errResponse);
+					});
 				}
 			};
 		} ]);
 
+classApp.factory('dataService', function() {
+	 var savedData = {}
+	 function set(data) {
+	   savedData = data;
+	 }
+	 function get() {
+	  return savedData;
+	 }
+
+	 return {
+	  set: set,
+	  get: get
+	 }
+	});
+
+
 classApp.controller('classController', [
 		'$scope',
 		'classService',
-		function($scope, classService) {
+		'dataService',
+		function($scope, classService, dataService) {
+			
+			$scope.selectedHEI = "Select";
+			$scope.selectedCourse = "Select";
+			$scope.selectedCourseYear = "Select";
+			
+			$scope.heis = [
+			    {name : "University of Leeds", code : "UNLE"},
+			    {name : "Manchester University", code : "MANU"},
+			    {name : "Glasgow University", code : "GLAU"}
+			];
+			
+			$scope.courses = [
+			    {name : "Biology", code : "12345"},
+			    {name : "Chemistry", code : "34567"},
+			    {name : "Mathematics", code : "25678"},
+			    {name : "Computer Science", code : "15680"}
+			];
+			
+			$scope.courseYears = [
+			    {name : "1", code : "1"},
+			    {name : "2", code : "2"},
+			    {name : "3", code : "3"}
+			];
 
 			$scope.isFieldsEmpty = false;
+			$scope.isAppFieldsEmpty = false;
+			
 			$scope.createCustomer = function() {
 
 				var firstName = $scope.firstName;
@@ -63,12 +117,14 @@ classApp.controller('classController', [
 					addressLine2 : addressLine2,
 					city : city,
 					postalCode : postalCode,
-					phoneNumber : phoneNumber
+					phoneNumber : phoneNumber,
+					createdBy : 'Class'
 				};
 				showOverLay('ProcessingOverlay', true, 6000);
 				classService.createCustomer(customer).then(
 						function(data) {
 							$scope.customerId = data.id;
+							dataService.set(data);
 							console.log("customer creation successful");
 							showOverLay('ProcessingOverlay', false, 6000);
 							showOverLay('CustomerCreatedOverlay', true, 6000);
@@ -76,6 +132,16 @@ classApp.controller('classController', [
 							console.error('Error while creating customer');
 							showOverLay('ProcessingOverlay', false, 6000);
 						});
+			}
+			
+			$scope.yes = function() {
+				showOverLay('CustomerCreatedOverlay',false, 6000);
+				location.href = "#createApplication";
+			}
+			
+			$scope.no = function() {
+				showOverLay('CustomerCreatedOverlay',false, 6000);
+				location.href = "http://localhost:9083/pages/classHome.html";
 			}
 
 			$scope.resetCustomer = function() {
@@ -90,8 +156,51 @@ classApp.controller('classController', [
 			}
 
 			$scope.ok = function() {
-				showOverLay('CustomerCreatedOverlay', false, 6000);
+				showOverLay('ApplicationCreatedOverlay', false, 6000);
 				location.href = "http://localhost:9083/pages/classHome.html";
+			}
+			
+			$scope.createApplication = function () {
+				var heiCode = $scope.selectedHEI;
+				var courseCode = $scope.selectedCourse;
+				var courseYear = $scope.selectedCourseYear;
+				var tflAmount = $scope.tflAmount;
+				var mlAmount = $scope.mlAmount;
+				
+				if (null == heiCode || heiCode == 'Select' || 
+					null == courseCode || courseCode == 'Select' || 
+					null == tflAmount || tflAmount == '' || 
+					null == mlAmount || mlAmount == '' || 
+					null == courseYear || courseYear == 'Select') 
+				{
+					$scope.isAppFieldsEmpty = true;
+					return;
+				} 
+			    $scope.isAppFieldsEmpty = false;
+			    
+			    var application = {
+			    		id : null,
+			    		customerId : dataService.get().id,
+			    		heiCode : heiCode,
+			    		courseCode: courseCode,
+			    		courseYear : courseYear,
+			    		tflAmount : tflAmount,
+			    		mlAmount : mlAmount,
+			    		createdBy : 'Class',
+			    		confirmedAtnInd : 'N'
+			    };
+			    showOverLay('ProcessingOverlay',true, 6000);
+			    classService.createApplication(application).then(
+				    function(data) {
+				    	$scope.applicationId = data.id;
+				    	console.log("application creation successful");
+				    	showOverLay('ProcessingOverlay',false, 6000);
+				    	showOverLay('ApplicationCreatedOverlay',true, 6000);
+					},
+					function(errResponse) {
+				        console.error('Error while creating application');
+				        showOverLay('ProcessingOverlay',false, 6000);
+					});
 			}
 
 		} ]);
